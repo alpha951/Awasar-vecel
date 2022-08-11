1f40918dec2181354e92b58884fd68973550d58f
Keshav Carpenter <20uec068@lnmiit.ac.in>
2022-08-11 14:17:02 +0530

    make it better

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.use(
         {
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: 'https://awasar.herokuapp.com//auth/google/jobly',
+            callbackURL: 'https://awasar.herokuapp.com/auth/google/jobly',
             userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
         },
         function (accessToken, refreshToken, profile, cb) {
