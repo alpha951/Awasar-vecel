de1550d972ef686be459a07405f8ce19579f5da1
Keshav Carpenter <20uec068@lnmiit.ac.in>
2022-12-17 22:56:47 +0530

    v6 some fixes ejs

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ const Job = require('./models/Job');
 const User = require('./models/User');
 const compression = require('compression');
 const app = express();
-app.set('view engine', 'ejs');
+
+
 app.use(compression()); //use compression 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 app.use(session({
     secret: process.env.SECRET,
