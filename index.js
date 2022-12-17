fe91525ace8aac036d25a7774f43e61bf02b2967
Keshav Carpenter <20uec068@lnmiit.ac.in>
2022-12-17 22:48:09 +0530

    v5 vercel all set

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(process.env.DBURL, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
